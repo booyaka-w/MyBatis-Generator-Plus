99ed23197c08d67eb17450ec82428e28f4a6fb87
Edge <Edge@Edge-PC>
2020-08-03 12:52:31 +0800

    初始导入

---
+++ src/main/java/com/booyaka/generator/MyBatisGeneratorPlusApplication.java
@@ -0,0 +1,13 @@
+package com.booyaka.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisGeneratorPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatisGeneratorPlusApplication.class, args);
+	}
+
+}
