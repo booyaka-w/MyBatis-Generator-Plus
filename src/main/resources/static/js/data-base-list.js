0b3236f70721e16427bf647758c6de9db87abedb
Edge <Edge@Edge-PC>
2020-08-05 20:59:29 +0800

    tableInfo

--- src/main/resources/static/js/data-base-list.js
+++ src/main/resources/static/js/data-base-list.js
@@ -5,17 +5,11 @@ function select(){
    	console.log(selects.options[indexs].text)
    	$table.bootstrapTable({
 		url : '/MyBatis-Generator-Plus/tables?dataBaseName='+selects.options[indexs].text,
-		showColumns : false,
-		showButtonIcons : false,
-		showExport: false,
-		showButtonText : false,
-		showRefresh : false,
-		showToggle: false,
 		cache: false,
-		detailView : false,
-		detailViewIcon: false,
-		detailViewByClick: false,
 		pagination: true,                   // 是否显示分页（*）
+		pageNumber: 1,						// 初始化加载第一页，默认第一页
+        pageSize: 1,                     	// 每页的记录行数（*）
+        sidePagination: "client",           // 分页方式：client客户端分页，server服务端分页（*）
         paginationPreText:'上一页',
         paginationNextText:'下一页',
         columns : [ {
@@ -31,9 +25,12 @@ function select(){
 			align : 'center',
 			width : 100,
 			formatter : function(value, row, index) {
-				return "<a class=\"text-info\" href=\"javascript:void(0)\" onclick=\"editModal('"+row.TABLE+"')\">详情</a> "
+				return "<a class=\"text-info\" href=\"javascript:void(0)\" onclick=\"toTableInfo('"+row.TABLE+"')\">详情</a> "
 			}
 		} ],
 	})
-    
+}
+
+function toTableInfo(tableName){
+
 }
